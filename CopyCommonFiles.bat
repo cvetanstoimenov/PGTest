b106576b655f7b9ef988b4afaf797f6b56eb8bb5
cvetan.stoimenov <cvetan.stoimenov@gmail.com>
2014-02-05 15:02:27 +0100

    first commit

---
+++ CopyCommonFiles.bat
@@ -0,0 +1,3 @@
+copy ..\WindowsPhone7\CordovaTwitterSearch\www\viewModel\*.js viewModel /Y
+copy ..\WindowsPhone7\CordovaTwitterSearch\www\lib\*.js lib /Y
+copy ..\WindowsPhone7\CordovaTwitterSearch\www\*.js . /Y
---
+++ app.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="intellisense.js" />
+
+/*globals $ document window $ ApplicationViewModel ko TwitterSearchViewModel*/
+
+$(document).ready(function () {
+  initializeViewModel();
+});
+
+var application;
+
+function initializeViewModel() {
+
+  application = new ApplicationViewModel();
+  ko.applyBindings(application);
+
+  var viewModel = new TwitterSearchViewModel();
+  viewModel.loadState();
+  application.navigateTo(viewModel);
+}
